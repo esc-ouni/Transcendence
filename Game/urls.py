b3d53b1d736658e8237383cd0163212270ac4ccc
esc-ouni <idouni@student.1337.ma>
2024-11-03 17:02:27 +0100

    New_Beginnings/Added

--- Game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index),
-    path('Game/', views.Game, name="PingP"),
-    # path("<str:name>", views.index_hi),
-]
