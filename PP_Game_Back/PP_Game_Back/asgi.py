754e5c4ba7fe848fc3b135b07e34fcceea9203cc
esc-ouni <idouni@student.1337.ma>
2024-11-09 12:09:23 +0100

    Back_End_Init/Init

---
+++ PP_Game_Back/PP_Game_Back/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PP_Game_Back project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PP_Game_Back.settings')
+
+application = get_asgi_application()
