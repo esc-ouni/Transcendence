0d2b9bf77c5ce0a84820f26f88326a5fa84e2026
esc-ouni <idouni@student.1337.ma>
2024-11-11 11:19:42 +0100

    Testttt/Init

--- PP_Game_Front/src/App.jsx
+++ PP_Game_Front/src/App.jsx
@@ -1,12 +1,12 @@
    // src/App.jsx
    import React from 'react';
-   import ThreeScene from './ThreeScene.jsx';
+   import ThreeGame from './ThreeScene.jsx';
    import './style.css';
 
    function App() {
      return (
        <div className="App">
-         <ThreeScene />
+         <ThreeGame />
        </div>
      );
    }
