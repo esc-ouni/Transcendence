2057986971f35308dd6a843f5a7621ec5108acde
esc-ouni <idouni@student.1337.ma>
2024-11-19 16:15:18 +0100

    Routers

---
+++ PP_Game_Front/src/Lobby.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+import { useNavigate } from "react-router-dom";
+
+function Lobby(){
+    const navigate = useNavigate();
+    return(<div className="yy">
+        <em>Lobby</em>
+        <button onClick={() => {navigate('/Threegame')}} > <em>Play Now</em></button>
+    </div>)
+}
+
+export default Lobby;
