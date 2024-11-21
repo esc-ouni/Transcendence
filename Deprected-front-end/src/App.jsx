fa0cf594aff96559cb0665be652fa8cb708c8a1c
esc-ouni <idouni@student.1337.ma>
2024-11-21 15:35:47 +0100

    Deprected-frontend

---
+++ Deprected-front-end/src/App.jsx
@@ -0,0 +1,22 @@
+   // src/App.jsx
+  //  import React from 'react';
+   import { Routes, Route } from 'react-router-dom';
+   import ThreeGame from './ThreeScene.jsx';
+   import Matchmaking from './Matchmaking.jsx';
+   import Lobby from './Lobby.jsx';
+   import './style.css';
+
+   function App() {
+     return (
+       <div className="App">
+          <Routes>
+            <Route path='/' element={<Lobby />}/>
+            <Route path='/Threegame' element={<ThreeGame />}/>
+            <Route path='/Threegame' element={<ThreeGame />}/>
+            {/* <Route path='/Matchmaking' element={<Matchmaking />}/> */}
+          </Routes>
+       </div>
+     );
+   }
+
+   export default App;
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,21 +1,35 @@
-   // src/App.jsx
-  //  import React from 'react';
-   import { Routes, Route } from 'react-router-dom';
-   import ThreeGame from './ThreeScene.jsx';
-   import Matchmaking from './Matchmaking.jsx';
-   import Lobby from './Lobby.jsx';
-   import './style.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-   function App() {
-     return (
-       <div className="App">
-          <Routes>
-            <Route path='/' element={<Lobby />}/>
-            <Route path='/Threegame' element={<ThreeGame />}/>
-            <Route path='/Matchmaking' element={<Matchmaking />}/>
-          </Routes>
-       </div>
-     );
-   }
+function App() {
+  const [count, setCount] = useState(0)
 
-   export default App;
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
