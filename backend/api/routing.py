e2451fd733b2af28a6e3384a77a8ac42e4feb2a9
esc-ouni <idouni@student.1337.ma>
2025-01-07 16:07:37 +0100

    Websockets
    server_routes_updates

--- backend/api/routing.py
+++ backend/api/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/server-endpoint-socket/', consumers.ApiConsumer.as_asgi())
+    re_path(r'ws/server-endpoint-socket/', consumers.ApiConsumer.as_asgi()),
+    re_path(r'ws/ping-pong/room/(?P<room_name>\w+)', consumers.GameRoomConsumer.as_asgi()) #test regex
 ]
--- frontend/src/game/Matchmaking.jsx
+++ frontend/src/game/Matchmaking.jsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from 'react';
 
 const WebSocketComponent = () => {
 const [connectionStatus, setConnectionStatus] = useState("Disconnected");
+const [Roomname, setRoomname] = useState("None");
 const [inputMessage, setInputMessage] = useState("");
+const [inputRMessage, setRInputMessage] = useState("");
 const [ready_socket, setSocket] = useState(null);
+const [Rroom_socket, setRSocket] = useState(null);
+const [my_id, setId] = useState(null);
 
 useEffect(() => {
   const socket = new WebSocket('ws://localhost:8000/ws/server-endpoint-socket/');
-
+  
   socket.onopen = () => {
     console.log("=> WebSocket Connected", socket);
     setConnectionStatus("Connected");
@@ -16,21 +20,45 @@ useEffect(() => {
   
   socket.onmessage = (event) => {
     const data = JSON.parse(event.data);
+    
+    if (data['type'] === 'connection_established'){
+      console.log((data['message']));
+      setId(data['my_id']);
+      console.log(data['my_id']);
+    }
 
     if (data['type'] === 'server_response')
       console.log("=> Message received:", data['message']);
+    
+    if (data['type'] === 'match_found'){
+      console.log("=> Match Infos:");
+      console.log("   => room_name   :", data['room_name']);
+      console.log("   => my_id       :", data['my_id']);
+      console.log("   => opponent_id :", data['opponent_id'],'\n');
 
+      setRoomname(data['room_name']);
+      if (!Rroom_socket){
+
+        let room_socket = new WebSocket('ws://localhost:8000/ws/ping-pong/room/bit_n3as/' + '/?user_id=' + data['my_id'])
+        // setRSocket(room_socket)
+        room_socket.onopen = () => {
+          console.log("=> Room WebSocket Connected", room_socket);
+          setRSocket(room_socket)
+        };
+        room_socket.onmessage = (event) => {
+          const data = JSON.parse(event.data);
+          
+            console.log("=> Type received :", data['type']);
+            
+            if (data['type'] === 'Game_State')
+              console.log("=> The brodcaster :", data['my_id']);
+              console.log("   => Says        :", data['message'], '\n');
+          }
+      }
+      
+    }
   };
 
-  socket.onerror = (error) => {
-    console.error('=> error:', error);
-  };
-
-  socket.onclose = () => {
-    console.log("=> WebSocket Disconnected");
-    setConnectionStatus("Disconnected");
-  };
-  
   return () => {
     console.log("=> Closing WebSocket On useEffect return !");
     socket.close();
@@ -52,6 +80,22 @@ const sendMessage = () => {
   }
 };
 
+const sendGameState = () => {
+  if (Rroom_socket && Rroom_socket.readyState === WebSocket.OPEN) {
+    const messageData = {
+      type: 'Game_State',
+      my_id: my_id,
+      message: inputRMessage,
+    };
+
+    Rroom_socket.send(JSON.stringify(messageData));
+
+    setRInputMessage('');
+  } else {
+    alert("Socket not ready!");
+  }
+};
+
 return (
   <div style={{padding: "20px", 
                fontFamily: "Arial, sans-serif"}}>
@@ -65,6 +109,15 @@ return (
         onChange={(e) => setInputMessage(e.target.value)}
       />
       <button onClick={sendMessage}>Send</button>
+    </div>
+    <p>Rom Connected to : <strong>{Roomname}</strong></p>
+    <div>
+      <input
+          type="text"
+          value={inputRMessage}
+          onChange={(e) => setRInputMessage(e.target.value)}
+        />
+      <button onClick={sendGameState}>Send Game State</button>
     </div>
   </div>
 );
